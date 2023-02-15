01117a19d10039c1717f0edffce21d9220d36c82
IngraPRJC <92542009+ConsumoCompania@users.noreply.github.com>
2023-02-14 22:50:23 -0300

    estilização e construção da lógica

---
+++ paginas/functions.js
@@ -0,0 +1,6 @@
+function erro(){
+   var explicacao = document.getElementById('explicacao')
+   var next = document.getElementById('next')
+   explicacao.style.display = 'block'
+   next.style.display = 'block'
+}
