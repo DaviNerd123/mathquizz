948e0bcfea9d7bdae8cadf4251aae126f3c8bb40
IngraPRJC <92542009+ConsumoCompania@users.noreply.github.com>
2023-02-16 22:24:06 -0300

    elaborando perguntas

--- paginas2/functions.js
+++ paginas2/functions.js
@@ -2,10 +2,14 @@ var acertos = 0
 function erro(){
    var explicacao = document.getElementById('explicacao')
    var next = document.getElementById('next')
+   document.getElementById('msge').innerHTML = "Que pena você errou ;-;"
    explicacao.style.display = 'block'
    next.style.display = 'block'
-   acertos = 0
+   document.getElementById('proxm').style.display = "inline"
+   
+   
 }
 function acerto(){
    acertos++
+   
 }
