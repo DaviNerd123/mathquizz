1bec09fe473e8357c5ded51ad92682ba0d12f04f
IngraPRJC <92542009+ConsumoCompania@users.noreply.github.com>
2023-02-18 17:50:32 -0300

    explicações

--- paginas/functions.js
+++ paginas/functions.js
@@ -1,12 +1,15 @@
 var acertos = 0
 function erro(){
+   var exp = String(document.getElementById('explicacao'))
+   var pag = document.getElementsById('body')
    var explicacao = document.getElementById('explicacao')
    var next = document.getElementById('next')
    document.getElementById('msge').innerHTML = "Que pena você errou ;-;"
    explicacao.style.display = 'block'
    next.style.display = 'block'
+   document.getElementById("body").innerHTML = "<p>o</p>"
 
-   
+
    
 }
 function erro2(){
